617dcef476e5355b03d1c7583f60613eea57f9ed
Asif Shah <unoneman666@gmail.com>
2026-09-21 11:29:26 +0530

    first commit!

---
+++ apps/frontend/app/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl">
+          hello
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
