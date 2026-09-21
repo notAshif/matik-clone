397fba12c66b10ded137305de70344876614f7cd
Asif Shah <unoneman666@gmail.com>
2026-09-21 13:28:33 +0530

    fix(database): rename package to @repo/database to match workspace convention

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express"
+import cors from "cors"
+import { AuthRoute } from "./routes/auth.router";
+
+const app = express();
+
+app.use(express.json())
+app.use(cors)
+
+app.post("/api/v1/auth", AuthRoute)
+
+app.listen(8000, () => console.log("server running at", 8000))
---
+++ apps/backend/routes/auth.router.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+export const AuthRoute = Router()
+
+AuthRoute.post("/register", (req, res) => {
+    //email, password
+    
+})
+AuthRoute.post("/login")
+AuthRoute.get("/me")
